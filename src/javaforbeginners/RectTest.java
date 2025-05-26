c231be7fb3c70e0ace66910194dd09a8644ad3ef
MSI\ichig <zeal24360155@fsgcl.jp>
2025-05-26 10:19:30 +0900

    色付き四角の追加

---
+++ src/javaforbeginners/RectTest.java
@@ -0,0 +1,23 @@
+package javaforbeginners;
+
+public class RectTest extends MyFrame {
+	
+ public void run() {
+	 
+	 setColor(255,255,0);
+	 fillRect(30,40,50,100);
+	 setColor(255,0,255);
+	 fillRect(130,40,50,100);
+	 setColor(0,255,255);
+	 fillRect(300,40,150,150);
+	 setColor(0,0,0);
+	 fillRect(30,200,500,500);
+	 setColor(128,128,128);
+	 fillRect(150,200,150,10);
+	 setColor(255,255,255);
+	 fillRect(50,215,100,100);
+	 
+	 
+	
+ }
+}
--- src/javaforbeginners/start.java
+++ src/javaforbeginners/start.java
@@ -4,7 +4,9 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-         System.out.println("こんにちは");
+         System.out.println("プログラムを実行します");
+         MyFrame frame1 = new RectTest();
+       
 	}
 
 }
