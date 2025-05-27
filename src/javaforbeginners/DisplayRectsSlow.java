8a946f3aed28502cad61f31c140dc1eb7b4373ed
MSI\ichig <zeal24360155@fsgcl.jp>
2025-05-27 10:45:04 +0900

    DisplayRectsSlowクラス＆SIeep

---
+++ src/javaforbeginners/DisplayRectsSlow.java
@@ -0,0 +1,16 @@
+package javaforbeginners;
+
+public class DisplayRectsSlow extends MyFrame{
+	public void run() {
+		setColor(0,128,0);
+		int x = 30;
+		int y = 80;
+		while(x<=170) {
+			fillRect (x,y,10,100);
+			x+=20;
+			y+=20;
+			sleep(1);
+		}
+	}
+
+}
--- src/javaforbeginners/start.java
+++ src/javaforbeginners/start.java
@@ -5,12 +5,13 @@ public class start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
          System.out.println("プログラムを実行します");
-         MyFrame frame1 = new RectTest();
+        /* MyFrame frame1 = new RectTest();
          MyFrame frame2 = new OvalTest();
          MyFrame frame3 = new TextTest();
          MyFrame frame4 = new RectAndOval();
          MyFrame frame5 = new RectLoop();
-         MyFrame frame6 = new RectIf();
+         MyFrame frame6 = new RectIf();*/
+         MyFrame frame7 = new  DisplayRectsSlow();
 	}
 
 }
